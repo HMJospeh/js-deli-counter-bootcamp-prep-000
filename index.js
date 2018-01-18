af610d122ef43187d9ef0ac8499c1dc8ee524911
Humza Joseph <humza1born@gmail.com>
2018-01-18 18:06:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeli,name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+function nowServing(deliLine) {
+  var cup = deliLine.shift()
+  if (deliLine.length == 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return `Currently serving ${cup}.`;
+  }
+}
+
+function currentLine(line) {
+  var 
+  if (line.length = 0) {
+    return "The line is currently empty.";
+  } else {
+    return `The line is currently: ${line}.`;
+  }
+}
+  
